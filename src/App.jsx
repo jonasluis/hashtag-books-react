193a65fb459dbb93ae8682a58ce9849c94052fc5
jonasluis <jonasluis66@gmail.com>
2025-03-06 13:49:54 -0300

    feat: arquivo para componente capa

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import './App.css'
 import brasCubasImg from './assets/bras_cubas.jpeg'
-
+import Capa from './Capa'
 function App() {
 
   const informacoesLivros = {
     nome: 'Memórias Póstumas de Brás Cubas',
     autor: 'Machado de Assis',
     totalCapitulos: 2,
-    capa: ""
+    capa: brasCubasImg,
+    textoAlternativo: 'Capa do livro Memórias Póstumas de Brás Cubas',
   }
 
   return(
     <>
-      <img src={brasCubasImg} alt="Capa do Livro." />
+      <Capa imagemCapa={informacoesLivros.capa} textoAlternativo={informacoesLivros.textoAlternativo} />
     </>
   )
 }
