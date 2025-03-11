972bdb18ef942187a79dc7fcaefe1b1b986c816f
jonasluis <jonasluis66@gmail.com>
2025-03-11 13:41:31 -0300

    feat: funcionalidade dos botões play e pause

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import brasCubasImg from './assets/bras_cubas.jpeg'
 import Capa from './Capa'
 import SeletorCapitulos from './SeletorCapitulos'
 import BotoesControle from './BotoesControle'
+import livro from './assets/capitulos/livro'
+import GerenciadorFaixa from './GerenciadorFaixa'
 
 function App() {
 
   //let taTocando = false;
   const [taTocando, definirTaTocando] = useState(false);
+  const [faixaAtual, definirFaixaAtual] = useState(0);
+const tagAudio = useRef(null)
   
 
   const informacoesLivros = {
@@ -17,15 +21,18 @@ function App() {
     autor: 'Machado de Assis',
     totalCapitulos: 2,
     capa: brasCubasImg,
+    capitulos: livro,
     textoAlternativo: 'Capa do livro Memórias Póstumas de Brás Cubas',
   }
 
   const tocarFaixa = () => {
     definirTaTocando(true);
+    tagAudio.current.play()
   }
-
+  
   const pausarFaixa = () => {
     definirTaTocando(false);
+    tagAudio.current.pause()
   }
 
   const tocarOuPausarFaixa = () => {
@@ -39,8 +46,9 @@ function App() {
 
   return(
     <>
-      <Capa imagemCapa={informacoesLivros.capa} textoAlternativo={informacoesLivros.textoAlternativo} />
+      <Capa imagemCapa={informacoesLivros.capa} textoAlternativo={informacoesLivros.textoAlternativo} faixa={informacoesLivros.capitulos[faixaAtual]}/>
       <SeletorCapitulos capituloAtual='1'/>
+      <GerenciadorFaixa faixa={informacoesLivros.capitulos[faixaAtual]} referencia={tagAudio}/>
       <BotoesControle 
       taTocando={taTocando} 
       tocarOuPausarFaixa={tocarOuPausarFaixa}
